72a60ed408d7e5e0e4b01497d5ea4fa62515973f
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2020-09-16 15:02:50 +0300

    chore: perform basic server configuration

---
+++ server.js
@@ -0,0 +1,33 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const typeDefs = gql`
+  type Query {
+    index: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    index: () => `Hi`
+  }  
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+(async () => {
+  await mongoose.connect('mongodb://localhost:27017/merng-blog', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+  });
+
+  console.log(`Database connected`);
+  
+  const { url } = await server.listen({ port: 5000 });
+
+  console.log(`Server running at ${url}`);
+})();
