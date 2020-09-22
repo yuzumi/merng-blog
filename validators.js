f30ffd7fffeea8d4ed1d80b3a496f7f325ab95c3
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2020-09-22 16:00:07 +0300

    chore(validators): add validator for login data

--- validators.js
+++ validators.js
@@ -31,5 +31,24 @@ module.exports = {
       errors,
       valid: Object.keys(errors).length === 0
     };
+  },
+  validateLoginInput(
+    username,
+    password
+  ) {
+    const errors = {};
+
+    if (username.trim() === "") {
+      errors.username = "Username must not be empty";
+    }
+
+    if (password.trim() === "") {
+      errors.password = "Password must not be empty";
+    } 
+
+    return {
+      errors,
+      valid: Object.keys(errors).length === 0
+    };
   }
 };
